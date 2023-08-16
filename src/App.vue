8470e400730c4c941955e4be99e35cfe3c3b8ec7
David Roesler <roeslerdavidthomas@gmail.com>
2023-08-16 13:16:08 -0700

    added todo creator

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { RouterView } from 'vue-router'
-import TodoHeader from "./components/TodoHeader.vue"
+import { RouterView } from 'vue-router';
+import TodoHeader from './components/TodoHeader.vue';
 </script>
 
 <template>
@@ -8,7 +8,7 @@ import TodoHeader from "./components/TodoHeader.vue"
   <RouterView />
 </template>
  
-<style lang="'scss'">
+<style lang="scss">
 
 @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap');
 * {
