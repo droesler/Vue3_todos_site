e0da2577a8928d4670b69ce733b7c5bea6e74c99
David Roesler <roeslerdavidthomas@gmail.com>
2023-08-16 12:32:56 -0700

    Finished L1

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import TodoHeader from "./components/TodoHeader.vue"
 </script>
 
 <template>
+  <TodoHeader />
   <RouterView />
 </template>
-
+ 
 <style lang="'scss'">
 
 @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap');
