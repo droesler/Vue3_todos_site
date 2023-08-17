cd305026c41a28a05ef70b761cfc742b5df9a84c
David Roesler <roeslerdavidthomas@gmail.com>
2023-08-17 10:48:21 -0700

    Finished L8

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 import { uid } from 'uid';
 import TodoCreator from '../components/TodoCreator.vue';
+import TodoItem from "../components/TodoItem.vue";
 const todoList = ref([]);
 
 const createTodo = (todo) => {
@@ -19,6 +20,9 @@ const createTodo = (todo) => {
   <main>
     <h1>Create Todo</h1>
     <TodoCreator @create-todo="createTodo" />
+    <ul class="todo-list">
+      <TodoItem v-for="todo in todoList" :todo="todo"/>
+    </ul>
   </main>
 </template>
 
@@ -35,5 +39,22 @@ main {
     margin-bottom: 16px;
     text-align: center;
   }
+
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
+  }
+  
 }
 </style>
